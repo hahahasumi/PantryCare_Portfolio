aad0eeabecaee29c14184ad7bf9b018b06c0e85b
hahahasumi <kotokotosityu@gmail.com>
2025-01-28 14:28:56 +0900

    初回コミット]

---
+++ src/main/java/com/example/demo/PantryCarePortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PantryCarePortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PantryCarePortfolioApplication.class, args);
+	}
+
+}
